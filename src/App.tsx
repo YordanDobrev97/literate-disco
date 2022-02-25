3a349f56392e69273dfd800e55556a870e010344
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-02-25 19:55:00 +0200

    Add some small changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Box } from '@mui/material'
 
 import { Navbar } from './components/navbar/Navbar'
 import { Home } from './pages/home/Home'
-import { Login } from './pages/auth/Login'
+import Login from './pages/auth/Login'
 import { Register } from './pages/auth/Register'
 import { ProjectList } from './pages/projects/ProjectList'
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import { Typography } from '@mui/material'
-
+import Login from '../auth/Login'
 export const Home = () => {
     return (
         <Typography variant="h2" component="h2" align="center">
             Welcome to 2NJ Projects
+
+            <Login />
         </Typography>
     )
 }
